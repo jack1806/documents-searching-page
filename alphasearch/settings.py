98a32ec82b97a7ac6d507deef6fe8a4032a1ba8a
decrypt <soberboidecrypt@gmail.com>
2019-07-13 16:31:22 +0530

    Allowed host

--- alphasearch/settings.py
+++ alphasearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*mj51b7*%eazt45((#&ffuv!@gj3l(^m!edi@y3o&q1-0^!p!@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [0.0.0.0]
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
